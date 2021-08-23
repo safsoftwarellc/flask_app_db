9ac56111fdc534f3e5011f4501b41ab037080089
Thulasi Duvvuru <thulasiatusa@gmail.com>
2021-08-22 23:11:20 -0400

    Excel Validations API added

--- main/controller/excel_validations_controller.py
+++ main/controller/excel_validations_controller.py
@@ -1,11 +1,11 @@
-from flask import flask, Blueprint, request, jsonify, send_file
+from flask import Blueprint, request, jsonify, send_file
 from werkzeug.utils import secure_filename
 from main.service.excel_db_service import (save_excel_info, update_excel_info, 
                                             get_excel_info, remove_excel_info,
                                             get_all_excel_info)
 from io import BytesIO
 
-excel_validations_app = Blueprint('queue_app', __name__)
+excel_validations_app = Blueprint('excel_validations_app', __name__)
 
 @excel_validations_app.route('/validationExcelFile', methods=['POST', 'PUT'])
 def saveValidationExcelFile():
@@ -16,7 +16,7 @@ def saveValidationExcelFile():
         return jsonify({'status':'file not selected!'})
     elif excel_file:
         s_filename=secure_filename(excel_file.filename)
-        if flask.request.method == 'PUT':
+        if request.method == 'PUT':
             return jsonify(update_excel_info(s_filename, excel_file))
         else:
             return jsonify(save_excel_info(s_filename, excel_file))
@@ -50,7 +50,7 @@ def getAllValidationExcelFilesInfo():
     all_files_info =  get_all_excel_info()
     files_info = {}
     for file_data in all_files_info:
-        files_info[file_data.file_id]={
+        files_info[file_data.data_id]={
         'file_id':file_data.data_id,
         'file_name':file_data.excel_file_name,
         'update_date':file_data.update_date}
